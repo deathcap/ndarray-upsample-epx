bd91cfc2a8550e7c69f3a252d36f0680dd23c7af
deathcap <deathcap@gmx.co.uk>
2014-03-02 19:41:17 -0800

    Update demo, show inp/out better

--- demo.js
+++ demo.js
@@ -12,17 +12,24 @@ var inp = ndarray(new Uint8Array([
         0, 0, 0, 0, 0]),
       [5, 5]);
 
+var show = function(a) {
+  for (var i = 0; i < a.shape[0]; i += 1) {
+    var s = '';
+    for (var j = 0; j < a.shape[1]; j += 1) {
+      s += a.get(i, j) ? '*' : '.';
+    }
+    console.log(s);
+  }
+};
+
 var out = zeros([10, 10]);
 
 epx(out, inp);
 
-for (var i = 0; i < out.shape[0]; i += 1) {
-  var s = '';
-  for (var j = 0; j < out.shape[1]; j += 1) {
-    s += out.get(i, j) + ' ';
-  }
-  console.log(s);
-}
+show(inp);
+console.log();
+show(out);
+
 
 //require('save-pixels')(inp, 'png').pipe(process.stdout);
 
